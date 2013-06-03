efa9eacda565843e624661bb83efcf160162774f
Jurgen <imperatus@gmail.com>
2013-06-03 02:53:54 +0200

    History

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel
             new Liber\UserBundle\LiberUserBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Bazinga\ExposeTranslationBundle\BazingaExposeTranslationBundle(),
+            new Ob\HighchartsBundle\ObHighchartsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
